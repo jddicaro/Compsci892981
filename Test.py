664f5f9d5e34a2d0bcc6e615c2b12f1b5124f458
Jojogottayeet <48447312+jddicaro@users.noreply.github.com>
2023-09-07 11:01:55 -0500

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,13 @@
+print("Welcome to G.R.P.! (Grade Retrieval Program)")
+name=input("\nLog in: ")
+stuid=input("\nStudent ID/Password: ")
+print("\nHello,",name+"!")
+ans1=input("Would you like to view a class? (Y/n)")
+if ans1=="n":
+	print("Why did you even start this program, then?)
+course=input("\nWhat class would you like to view?\n")
+assignment=input("\nWhat assignment would you like to look at?\n")
+input("Please give me a moment while I retrieve your info... (ENTER to continue)")
+print("Here you go!\n\n ",course,"\n\n",assignment,"\n\n",name,"\t\t",stuid,"\n\n Grade: \nComments: \nAssignment correctness: \nQuality of style: \nLate deduction: \nOverall score: \nComments: ")
+input("(ENTER to continue)")
+print("Thank you for using G.R.P.!")
