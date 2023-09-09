4eaa88f52948f78f3490c3e53e5fe53002c9b8fb
Jojogottayeet <48447312+jddicaro@users.noreply.github.com>
2023-09-09 16:24:16 -0500

    Update gradingForm_AI-Revision.py

--- Python/gradingForm_AI-Revision.py
+++ Python/gradingForm_AI-Revision.py
@@ -1,9 +1,10 @@
-#THIS CODE WAS REVISED BY AI, PLEASE DO NOT TAKE AS A GRADE!
-#Well, I did add a bit to it after and forgot to save that to a new file, so i'll just comment where my code is
+# THIS CODE WAS REVISED BY AI, PLEASE DO NOT TAKE AS A GRADE!
+# Well, I did add a bit to it after and forgot to save that to a new file, so I'll just comment where my code is
 
-#AI
+# AI
 def query_yes_no(question):
-	"""Ask a yes/no question via standard input and return the answer. If invalid input is given, the user will be asked until they acutally give valid input.
+    """Ask a yes/no question via standard input and return the answer. If invalid input is given, the user will be
+    asked until they actually give valid input.
 
 	Args:
 		question(str): A question that is presented to the user.
@@ -14,46 +15,51 @@ def query_yes_no(question):
 	Side Effects:
 		Blocks program execution until valid input(y/n) is given.
 	"""
-	yes_list = ["yes", "y"]
-	no_list = ["no", "n"]
-	prompt = question + " (y/n) "
-	while True:
-		choice = input(prompt).lower()
-		if choice in yes_list:
-			return True
-		elif choice in no_list:
-			return False
-		else:
-			print("Please respond with 'y' or 'n'")
+    yes_list = ["yes", "y", ""]
+    no_list = ["no", "n"]
+    prompt = question + " (Y/n) "
+    while True:
+        choice = input(prompt).lower()
+        if choice in yes_list:
+            return True
+        elif choice in no_list:
+            return False
+        else:
+            print("Please respond with 'y' or 'n'")
 
-#me
+
+# me
 print("[THIS CODE WAS REVISED BY AI, PLEASE DO NOT TAKE AS A GRADE]")
 print("Welcome to G.R.P.! (Grade Retrieval Program)")
 name = input("\nLog in: ")
-stuid = input("\nStudent ID/Password: ")
+stu_id = input("\nStudent ID/Password: ")
 print("\nHello,", name + "!")
 
 ans1 = query_yes_no("Would you like to view a class? ")
 
 if ans1:
-	course = input("\nWhat class would you like to view?\n")
-	assignment = input("\nWhat assignment would you like to look at?\n")
-	input("Please give me a moment while I retrieve your info... (ENTER to continue)")
-	print("Here you go!\n\n", course, "\n\n", assignment, "\n\n", name, "\t\t", stuid, "\n\n Grade: \nComments: \nAssignment correctness: \nQuality of style: \nLate deduction: \nOverall score: \nComments: ")
-	input("(ENTER to continue)")
-	while True:
-		#I added this while true loop after looking at what Bard did at the beginning, bc I thought it was cool
-		ans2 = query_yes_no("Would you like to check another grade?")
-		if ans2: 
-			course = input("\nWhat class would you like to view?\n")
-			assignment = input("\nWhat assignment would you like to look at?\n")
-			input("Please give me a moment while I retrieve your info... (ENTER to continue)")
-			print("Here you go!\n\n", course, "\n\n", assignment, "\n\n", name, "\t\t", stuid, "\n\n Grade: \nComments: \nAssignment correctness: \nQuality of style: \nLate deduction: \nOverall score: \nComments: ")
-			input("(ENTER to continue)")
-		else:
-			print("Thank you for using G.R.P.!\n")
-			break
+    course = input("\nWhat class would you like to view?\n")
+    assignment = input("\nWhat assignment would you like to look at?\n")
+    input("Please give me a moment while I retrieve your info... (ENTER to continue)")
+    print("Here you go!\n\n", course, "\n\n", assignment, "\n\n", name, "\t\t", stu_id,
+          "\n\n Grade: \nComments: \nAssignment correctness: \nQuality of style: "
+          "\nLate deduction: \nOverall score: \nComments: ")
+    input("(ENTER to continue)")
+    while True:
+        # I added this while true loop after looking at what Bard did at the beginning, bc I thought it was cool
+        ans2 = query_yes_no("Would you like to check another grade?")
+        if ans2:
+            course = input("\nWhat class would you like to view?\n")
+            assignment = input("\nWhat assignment would you like to look at?\n")
+            input("Please give me a moment while I retrieve your info... (ENTER to continue)")
+            print("Here you go!\n\n", course, "\n", assignment, "\n", name, "\t", stu_id,
+                  "\n Grade: \n Comments: \n Assignment correctness: \n Quality of style: "
+                  "\n Late deduction: \n Overall score: \n Comments: ")
+            input("(ENTER to continue)")
+        else:
+            print("Thank you for using G.R.P.!\n")
+            break
 else:
-	print("Why did you even start this program, then?\n")
+    print("Why did you even start this program, then?\n")
 
 input("Press ENTER to close program.")
